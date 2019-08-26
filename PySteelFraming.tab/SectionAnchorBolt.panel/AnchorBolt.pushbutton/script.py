cc29ff972887db724d14351c78c3609047608364
=Nhuan <=nguyenvannhuan90123@gmail.com>
2019-08-26 13:58:04 +0700

    update

--- PySteelFraming.tab/SectionAnchorBolt.panel/AnchorBolt.pushbutton/script.py
+++ PySteelFraming.tab/SectionAnchorBolt.panel/AnchorBolt.pushbutton/script.py
@@ -16,19 +16,17 @@ uidoc = rpw.revit.uidoc  # type: UIDocument
 doc = rpw.revit.doc  # type: Document
 logger = script.get_logger()
 
-
 class SectionTypeSelection(forms.WPFWindow):
     def __init__(self):
         forms.WPFWindow.__init__(self, "SectionTypeSelection.xaml")
 
         self.combob_selector.DataContext = \
-            [vt for vt in FilteredElementCollector(doc).OfClass(ViewFamilyType) if vt.ViewFamily == ViewFamily.Section]
-
+            [vt for vt in FilteredElementCollector(doc).OfClass(ViewFamilyType) if vt.FamilyName == "Structural Plan"]
+        self.Input_Parameter.DataContext = self.Input_Parameter.Text
         self.selected_type = None
-    def select_click(self, sender, e):
+    def Button_Click(self, sender, e):
         self.selected_type = self.combob_selector.SelectedItem
         self.Close()
-
     def show_dialog(self):
         self.ShowDialog()
         if self.selected_type:
@@ -36,8 +34,8 @@ class SectionTypeSelection(forms.WPFWindow):
         else:
             import sys
             sys.exit()
-section_type = SectionTypeSelection().show_dialog()
 
+section_type = SectionTypeSelection().show_dialog()
 collectors = FilteredElementCollector(doc).OfClass(ViewFamilyType)
 _config = script.get_config()
 prefix = _config.get_option('prefix', 'Mur')
@@ -50,23 +48,23 @@ Y_Bottom = float(_config.get_option('Y_Bottom', '1'))
 t = Transaction(doc, 'This is my new transaction')
 t.Start()
 # Select an element in Revit
-picked = uidoc.Selection.PickObject(ObjectType.Element, "Select something.")
-eleid = picked.ElementId
-ele = doc.GetElement(eleid)
-# Get bounding box of selected element.
-#picked_bb = BoundingBoxXYZ()
-picked_bb = ele.get_BoundingBox(doc.ActiveView)  
+for element_id in uidoc.Selection.GetElementIds():
+    ele = doc.GetElement(element_id) 
+    picked_bb = ele.get_BoundingBox(doc.ActiveView)  
 # Get max and min points of bounding box.
-picked_bb_max = picked_bb.Max
-picked_bb_min = picked_bb.Min
-for collector in collectors:
-    tn = Element.Name.__get__(collector)
-    if tn == 'IIa.Detail Anchor Bolt':
-        ViewFamilyTypeId = collector.Id     
-Point1 = XYZ(picked_bb_min.X -X_Left,picked_bb_min.Y - Y_Bottom,0)
-Point2 = XYZ(picked_bb_max.X +X_Right,picked_bb_max.Y + Y_Up,0)
-ViewParent = doc.ActiveView
-ViewParentid = ViewParent.Id
-view = ViewSection.CreateCallout (doc,ViewParentid,ViewFamilyTypeId,Point1,Point2)
-ColViews = FilteredElementCollector(doc).OfClass(View)
+    picked_bb_max = picked_bb.Max
+    picked_bb_min = picked_bb.Min
+    for collector in collectors:
+        tn = Element.Name.__get__(collector)
+        if tn == 'IIa.Detail Anchor Bolt':
+            ViewFamilyTypeId = collector.Id     
+    Point1 = XYZ(picked_bb_min.X -X_Left,picked_bb_min.Y - Y_Bottom,0)
+    Point2 = XYZ(picked_bb_max.X +X_Right,picked_bb_max.Y + Y_Up,0)
+    ViewParent = doc.ActiveView
+    ViewParentid = ViewParent.Id
+    view = ViewSection.CreateCallout (doc,ViewParentid,ViewFamilyTypeId,Point1,Point2)
+    view.Name = Input_Parameter
+    ColViews = FilteredElementCollector(doc).OfClass(View)
+    #for ColView in ColViews:
+        #print (ColView.Name)
 t.Commit()
---
+++ PySteelFraming.tab/SectionAnchorBolt.panel/Rwp.pushbutton/script.py
@@ -0,0 +1,23 @@
+__doc__ = "Test run Form with button"
+__author__ = 'HO VAN CHUONG'
+__title__ = 'Form Option\nRWP'
+# -*- coding: UTF-8 -*-
+from Autodesk.Revit.DB import Transaction, BuiltInParameter, Element, Level, MEPCurve, ElementId, FamilyInstance\
+    , FilteredElementCollector
+from Autodesk.Revit.UI import IExternalEventHandler, ExternalEvent
+from pyrevit.forms import WPFWindow
+from event import CustomizableEvent
+import clr
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
+import System.Drawing
+import System.Windows.Forms
+def clicked():
+     print("Hello work")
+class WPF_PYTHON(WPFWindow):
+    def __init__(self, xaml_file_name):
+        WPFWindow.__init__(self, xaml_file_name)
+    def button1_Click(self, sender, event):
+        #ext_event.Raise()
+        clicked()
+WPF_PYTHON = WPF_PYTHON('WPF_PYTHON.xaml').Show()
