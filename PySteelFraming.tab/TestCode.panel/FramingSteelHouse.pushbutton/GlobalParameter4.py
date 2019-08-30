3d540b14f5f131cc461dec47c467b6a967131d2a
=Nhuan <=nguyenvannhuan90123@gmail.com>
2019-08-30 11:08:57 +0700

    up

--- PySteelFraming.tab/TestCode.panel/FramingSteelHouse.pushbutton/GlobalParameter4.py
+++ PySteelFraming.tab/TestCode.panel/FramingSteelHouse.pushbutton/GlobalParameter4.py
@@ -12,8 +12,6 @@ class Global:
     def globalparameterchange(self,TypeElement):
         #t = Transaction (doc,"Slope Element")
         #t.Start()
-        
-
         paramId = GlobalParametersManager.FindByName(doc,"Slope")
         param = doc.GetElement(paramId) 
         kkkk = ConvertToInternalUnits1(self.ParameterValue)
@@ -33,20 +31,3 @@ class ConvertToInternalUnits1:
         #print (self.ParameterValue)
         ParameterValue = UnitUtils.ConvertToInternalUnits(self.ParameterValue, DisplayUnitType.DUT_DECIMAL_DEGREES)
         return ParameterValue
-    
-
-
-"""
-def ConvertToInternalUnits(UNIT):
-    ParameterValue = UnitUtils.ConvertToInternalUnits(UNIT, DisplayUnitType.DUT_MILLIMETERS)
-    return ParameterValue
-def GlobalParameter():
-    t = Transaction (doc,"Slope Element")
-    t.Start()
-    paramId = GlobalParametersManager.FindByName(doc, "Slope")
-    param = doc.GetElement(paramId) 
-    paramtype = type(param)
-    ParameterValue = UnitUtils.ConvertToInternalUnits(20, DisplayUnitType.DUT_DECIMAL_DEGREES)
-    param.SetValue (DoubleParameterValue(ParameterValue))
-    t.Commit()
-"""
