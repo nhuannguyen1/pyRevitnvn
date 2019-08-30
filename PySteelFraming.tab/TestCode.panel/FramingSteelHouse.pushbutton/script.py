ef98382d106801740fbce6771b54508f7429a170
=Nhuan <=nguyenvannhuan90123@gmail.com>
2019-08-30 19:51:12 +0700

    add

--- PySteelFraming.tab/TestCode.panel/FramingSteelHouse.pushbutton/script.py
+++ PySteelFraming.tab/TestCode.panel/FramingSteelHouse.pushbutton/script.py
@@ -17,40 +17,17 @@ from pyrevit.forms import WPFWindow, alert
 
 #Get Family Symbol
 
-def PlaceElement (Base_Leveled,Base_Leveled_Point,Column_Typed,Top_Leveled,Slope_Type,XYZConnection,ConnectionType):
+def PlaceElement (Base_Leveled,Base_Leveled_Point,Column_Typed,Top_Leveled,Slope_Type):
     t = Transaction (doc,"Place Element")
     t.Start()
-    #ColumnCreate = doc.Create.NewFamilyInstance(XYZConnection, ConnectionType,XYZ_Direction,Element_Host, Structure.SNonStructural)
     ColumnCreate = doc.Create.NewFamilyInstance(Base_Leveled_Point, Column_Typed,Base_Leveled, Structure.StructuralType.Column)
     a= Global(Slope_Type)
-
-    
-
     a.globalparameterchange(ColumnCreate)
     paramerTopLeve = ColumnCreate.get_Parameter(BuiltInParameter.FAMILY_TOP_LEVEL_PARAM)
     paramerTopLeve.Set(Top_Leveled.Id)
     TopoffsetPam = ColumnCreate.get_Parameter(BuiltInParameter.FAMILY_TOP_LEVEL_OFFSET_PARAM)
     TopoffsetPam.Set(0)
-    
-    print (ColumnCreate)
-    CreateElementHost (XYZConnection, ConnectionType,ColumnCreate)
-
     t.Commit()
-
-def CreateElementHost(XYZConnection, ConnectionType,element_Host):
-    #t = Transaction (doc,"Place Element host")
-    #t.Start()
-    ConectionPlate = doc.Create.NewFamilyInstance(XYZConnection, ConnectionType, element_Host, Structure.StructuralType.NonStructural)
-    """
-    a= Global(Slope_Type)
-    a.globalparameterchange(ColumnCreate)
-    paramerTopLeve = ColumnCreate.get_Parameter(BuiltInParameter.FAMILY_TOP_LEVEL_PARAM)
-    paramerTopLeve.Set(Top_Leveled.Id)
-    TopoffsetPam = ColumnCreate.get_Parameter(BuiltInParameter.FAMILY_TOP_LEVEL_OFFSET_PARAM)
-    TopoffsetPam.Set(0)
-    """
-   # t.Commit()
-   
 def Getintersection (line1, line2):
     results = clr.Reference[IntersectionResultArray]()
     result = line1.Intersect(line2, results)
@@ -98,9 +75,7 @@ class WPF_PYTHON(WPFWindow):
         print (Plate_Connection_Type_Lefted)
         XYZConnection = XYZ (2,2,2)
         # place column to project 
-        PlaceElement(Base_Leveled,Base_Leveled_Point,Column_Typed,Top_Leveled,Slope_T,XYZConnection,Plate_Connection_Type_Lefted)
+        PlaceElement(Base_Leveled,Base_Leveled_Point,Column_Typed,Top_Leveled,Slope_T)
         #plate Element to host 
-       
-
         self.Close()
 WPF_PYTHON = WPF_PYTHON('WPF_PYTHON.xaml').ShowDialog()
