aed56243dba50b87801c838f7896ad41322b750e
=Nhuan <=nguyenvannhuan90123@gmail.com>
2019-09-02 00:23:44 +0700

    aaaaaaaaaa

--- PySteelFraming.tab/TestCode.panel/FramingSteelHouse.pushbutton/script.py
+++ PySteelFraming.tab/TestCode.panel/FramingSteelHouse.pushbutton/script.py
@@ -55,7 +55,6 @@ def Getintersection (line1, line2):
 	    print('No Intesection')
     res = results.Item[0]
     return res.XYZPoint
-<<<<<<< HEAD
 def writefilecsv(Cout_Continue,Rafter_Family_Lefted,Rafter_Type_Lefted,Length_Rater_Lefted_n):
     t = Transaction(doc, 'Write an external file.')
     t.Start()
@@ -72,8 +71,6 @@ def writefilecsv(Cout_Continue,Rafter_Family_Lefted,Rafter_Type_Lefted,Length_Ra
     readFile.close()
     writeFile.close()
     t.Commit()
-=======
->>>>>>> parent of 1487996... addd
 class WPF_PYTHON(WPFWindow):
     def __init__(self, xaml_file_name):
         WPFWindow.__init__(self, xaml_file_name)
