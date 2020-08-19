fac055a83e03f068b7ab28191ba1e710f5ce8ef9
Nguyen Van Nhuan <45336894+nhuannguyen1@users.noreply.github.com>
2020-08-19 13:09:21 +0700

    all

--- PySteelFraming.tab/traning.panel/traning.pushbutton/script.py
+++ PySteelFraming.tab/traning.panel/traning.pushbutton/script.py
@@ -1,36 +1,53 @@
 __doc__ = 'Create gird by parameter from excel file'
 __author__ = 'Nguyen Van Nhuan'
 __title__ = 'DGrid'
-import xlrd
-import os
+import xlrd,os
 from Autodesk.Revit.DB import Transaction
 from pyrevitnvn.draw.gird import dgrid
-from rpw.ui.forms import FlexForm, Button
-from System.Windows import Window
+from pyrevitnvn.draw import draw
+
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
+
+# retrieve active view 
 view = doc.ActiveView
-# retrieve dir path from fullname 
+
+# retrieve dir path from abspath 
 dir_path = os.path.dirname(os.path.abspath(__file__))
-# retrieve directory excel file 
+
+# retrieve fullname excel file 
 file_loc =os.path.join(dir_path,"Create_Grids.xlsx")
+
 # open wb by xlrd 
 workbook = xlrd.open_workbook(filename=file_loc)
 
 # retrieve sheet excel  by index
 sheet = workbook.sheet_by_index(0)
 
-# retreve value cell from excel
+# retreve value cell from excel, starting coordinates of
+# horizontal(Origin_Coordy) and vertical (Origin_Coordx)
 Origin_Coordx = eval(sheet.cell_value(0,1))
 Origin_Coordy = eval(sheet.cell_value(0,4))
 
+@draw(file_loc)
 def run():
     """ 
-    drawing gird by parameter from excel 
-
+    drawing gird by parameter from excel\n
+    name_text_gird: 
+    name_text_gird: colum index from  excel to retrieve text gird \n
+    dist_gird_col: distance of gird \n
+    length_gird_col: length of gird \n
+    type: horizontal or vertical \n  
+          the direction axis \n
+          default value: vertical \n
+    sheet: worksheet input \n
+    coord_start: starting coordinates
     """
+    # context-like objects that guard any changes made to a Revit model
     t = Transaction(doc, "Create grids")
+    # Starts the transaction.
     t.Start()
+    # drawing gird to project revit 
     dgrid(name_text_gird="D",
           dist_gird_col="E",
           length_gird_col="F",
@@ -38,60 +55,13 @@ def run():
           coord_start=Origin_Coordy,
           sheet=sheet
           )
+    # drawing gird to project revit 
     dgrid(name_text_gird="A",
           dist_gird_col="B",
           length_gird_col="C",
           coord_start=Origin_Coordx,
           sheet=sheet
           )
+    # Commits all changes made to the model during the transaction.
     t.Commit()
-
-class dgrid:
-    def __init__(self,f):
-        self.f = f
-    
-    def fgui(self):
-        components = [Button("Open Excel",
-                     on_click=openwb_conf),
-        Button("run",
-                     on_click= run_gird)
-             ]
-    def openwb_conf(self,sender, e):
-        """
-        open excel file by input path
-        """
-        os.startfile(file_loc)
-    def run_gird(self,sender, e):
-        """
-        drawing gird by parameter from excel 
-        """
-        run()
-    
-    def __call__(self):
-
-
-
-
-
-
-
-
-
-def openwb_conf(sender, e):
-    """
-    open excel file by input path
-    """
-    os.startfile(file_loc)
-def run_gird(sender, e):
-    """
-    drawing gird by parameter from excel 
-    """
-    run()
-#UI form organization
-components = [Button("Open Excel",
-                     on_click=openwb_conf),
-              Button("run",
-                     on_click= run_gird)
-             ]
-form = FlexForm('DGrid', components) 
-form.show() 
+run()
---
+++ lib/pyrevitnvn/draw/__init__.py
@@ -0,0 +1,30 @@
+from rpw.ui.forms import FlexForm, Button
+import os
+
+class draw:
+    def __init__(self,filename):
+        self.filename = filename
+    
+    def fgui(self):
+        self.components = [Button("Open Excel",
+                             on_click=self.openwb_conf),
+                      Button("run",
+                             on_click= self.run_gird)
+                    ]
+    def openwb_conf(self,sender, e):
+        """
+        open excel file by input path
+        """
+        os.startfile(self.filename)
+    def run_gird(self,sender, e):
+        """
+        drawing gird by parameter from excel 
+        """
+        self.f()
+    def __call__(self,f):
+        self.f  = f
+        def wrapped_f(*args):
+            self.fgui()
+            form = FlexForm('DGrid', self.components) 
+            form.show() 
+        return wrapped_f 
