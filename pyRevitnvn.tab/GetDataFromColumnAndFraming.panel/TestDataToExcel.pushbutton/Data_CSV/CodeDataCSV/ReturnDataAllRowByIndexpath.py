b58bdd8b6ca82452d661bc65a176cc8d3f22404b
Nguyen Van Nhuan <45336894+nhuannguyen1@users.noreply.github.com>
2020-08-19 15:24:36 +0700

    all

--- pyRevitnvn.tab/GetDataFromColumnAndFraming.panel/TestDataToExcel.pushbutton/Data_CSV/CodeDataCSV/ReturnDataAllRowByIndexpath.py
+++
@@ -1,9 +0,0 @@
-import csv
-def ReturnDataAllRowByIndexpath (path,NumberRow):
-        with open(path) as csvFile:
-            readcsv =csv.reader(csvFile, delimiter=',')
-            readcsv = list(readcsv)
-            RowNumber = readcsv[NumberRow]
-        csvFile.close()
-        RowNumber.pop(0)
-        return RowNumber
--- pyRevitnvn.tab/GetDataFromColumnAndFraming.panel/TestDataToExcel.pushbutton/Data_CSV/CodeDataCSV/Test.py
+++
@@ -1,5 +0,0 @@
-matr_= [[1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]]
-rotated90 = zip(*matr_[::1])
-print (list(rotated90))
