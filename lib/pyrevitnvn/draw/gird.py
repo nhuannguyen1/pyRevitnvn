30b1b407c5bfaa31ad75a07adeb94e300c2bc747
Nguyen Van Nhuan <45336894+nhuannguyen1@users.noreply.github.com>
2020-08-23 23:37:18 +0700

    all

--- lib/pyrevitnvn/draw/gird.py
+++ lib/pyrevitnvn/draw/gird.py
@@ -1,4 +1,3 @@
-
 import re
 from Autodesk.Revit.DB import (XYZ,
                                 Line,
@@ -13,38 +12,47 @@ from pyrevitnvn.units import Convert_length
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document   
 
-def dgrid(name_text_gird = "A", 
-            dist_gird_col = 2000, 
-            length_gird_col = 300, 
+def dgrid(name_text_gird_col = "A", 
+            dist_gird_col = "B", 
+            length_gird_col = "C", 
             coord_start = (0,0,0),
-            type = "vertical",
+            type_d = "vertical",
             sheet = None
             ):
     """
     create grid to project \n
-    name_text_gird: colum index from  excel to retrieve text gird \n
+    name_text_gird_col: colum index from  excel to retrieve text gird \n
     dist_gird_col: distance of gird \n
     length_gird_col: length of gird \n
     """
     # create dict from range 
     dicta = dby_Lindex_col(sheet=sheet,
-                        key_index_column=name_text_gird,
-                        value_index_cols=[dist_gird_col,length_gird_col])
-    #dictb = dby_Lindex_col(sheet=sheet,key_index_column="D",value_index_cols=[dist_gird,length_gird])
+                           key_index_column=name_text_gird_col,
+                           value_index_cols=[dist_gird_col,length_gird_col])
+
+    # sort list by number 
     lkey = sorted(list(dicta.keys()),key=lambda x: int((re.findall('\d+', x ))[0]))
 
-    c = coord_start[0] if type == "vertical" else coord_start[1] 
+    # check verical or horzontal 
+    c = coord_start[0] if type_d == "vertical" else coord_start[1] 
+
+    # retrieve of all distance 
+    sum_total_dis = Convert_length(sum ([dicta[key][0]  for key in lkey ]))
+
     # drawing gird x vertical 
     for key in lkey:
         # retrieve distance and length 
         dis,length = dicta[key]
+
         # convert unit for dis
         dis = Convert_length(dis)
-        # convert unit for distance
-        length = Convert_length(length)
+        
+        # convert unit for length
+        length = Convert_length(length) if type(length)== float else sum_total_dis
+
         dis = c + dis
         
-        if type == "vertical":
+        if type_d == "vertical":
             # coord start 
             scoord = XYZ(dis,0,0)
             # coord start 
@@ -55,19 +63,103 @@ def dgrid(name_text_gird = "A",
             # coord start 
             ecoord = XYZ(length,dis,0)
         c= dis
+
         # context-like objects that guard any 
         # changes made to a Revit model
         t = Transaction(doc, "Create grids")
+
         # Starts the transaction.
         t.Start()
+
         # create line reference 
         line_ref = Line.CreateBound(scoord, ecoord)
+
         # create gird  
         grid_ins = Grid.Create(doc, line_ref)
+
         # retrieve name for gird 
         name = grid_ins.get_Parameter(BuiltInParameter.DATUM_TEXT)
+
         # set name for gird 
         name.Set(key)
+
+        # Commits all changes made 
+        # to the model during the transaction.
+        t.Commit()
+def d2grid(ver_name_text_gird_col = "A", 
+           ver_dist_gird_col = "B", 
+           ver_length_gird_col = "C", 
+           ver_coord_start = (0,0,0),
+           hor_name_text_gird_col = "A", 
+           hor_dist_gird_col = "B", 
+           hor_length_gird_col = "C", 
+           hor_coord_start = (0,0,0),
+            sheet = None
+            ):
+    """
+    create grid to project \n
+    name_text_gird_col: colum index from  excel to retrieve text gird \n
+    dist_gird_col: distance of gird \n
+    length_gird_col: length of gird \n
+    """
+    # create dict from range 
+    dicta = dby_Lindex_col(sheet=sheet,
+                           key_index_column=name_text_gird_col,
+                           value_index_cols=[dist_gird_col,length_gird_col])
+
+    # sort list by number 
+    lkey = sorted(list(dicta.keys()),key=lambda x: int((re.findall('\d+', x ))[0]))
+
+    # check verical or horzontal 
+    c = coord_start[0] if type_d == "vertical" else coord_start[1] 
+
+    # retrieve of all distance 
+    sum_total_dis = Convert_length(sum ([dicta[key][0]  for key in lkey ]))
+
+    # drawing gird x vertical 
+    for key in lkey:
+        # retrieve distance and length 
+        dis,length = dicta[key]
+
+        # convert unit for dis
+        dis = Convert_length(dis)
+        
+        # convert unit for length
+        length = Convert_length(length) if type(length)== float else sum_total_dis
+
+        dis = c + dis
+        
+        if type_d == "vertical":
+            # coord start 
+            scoord = XYZ(dis,0,0)
+            # coord start 
+            ecoord = XYZ(dis,length,0)
+        else:
+            # coord start 
+            scoord = XYZ(0,dis,0)
+            # coord start 
+            ecoord = XYZ(length,dis,0)
+        c= dis
+
+        # context-like objects that guard any 
+        # changes made to a Revit model
+        t = Transaction(doc, "Create grids")
+
+        # Starts the transaction.
+        t.Start()
+
+        # create line reference 
+        line_ref = Line.CreateBound(scoord, ecoord)
+
+        # create gird  
+        grid_ins = Grid.Create(doc, line_ref)
+
+        # retrieve name for gird 
+        name = grid_ins.get_Parameter(BuiltInParameter.DATUM_TEXT)
+
+        # set name for gird 
+        name.Set(key)
+
         # Commits all changes made 
         # to the model during the transaction.
         t.Commit()
--- pyRevitnvn.tab/Training.panel/Training.pushbutton/script.py
+++ pyRevitnvn.tab/Training.panel/Training.pushbutton/script.py
@@ -3,8 +3,8 @@ __author__ = 'Nguyen Van Nhuan - pyan.vn'
 __title__ = 'DGrid'
 import xlrd,os
 from Autodesk.Revit.DB import Transaction
-from pyrevitnvn.draw.gird import dgrid
 from pyrevitnvn.draw import draw
+from pyrevitnvn.draw.gird import dgrid
 
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
@@ -20,5 +20,30 @@ file_loc =os.path.join(dir_path,"Create_Grids.xlsx")
 
 # open wb by xlrd 
 workbook = xlrd.open_workbook(filename=file_loc)
+
 # retrieve sheet excel  by index
 sheet = workbook.sheet_by_index(0)
+
+# retreve value cell from excel, starting coordinates of
+# horizontal(ocy) and vertical (ocx)
+ocx = eval(sheet.cell_value(0,1))
+ocy = eval(sheet.cell_value(0,4))
+
+@draw(file_loc)
+def run():
+    """ 
+    drawing gird by parameter from excel\n
+    name_text_gird: 
+    name_text_gird: colum index from  excel to retrieve text gird \n
+    dist_gird_col: distance of gird \n
+    length_gird_col: length of gird \n
+    type: horizontal or vertical \n  
+          the direction axis \n
+          default value: vertical \n
+    sheet: worksheet input \n
+    coord_start: starting coordinates
+    """
+    # drawing gird to project revit 
+    dgrid(name_text_gird_col="A",dist_gird_col="B",length_gird_col="C",coord_start=ocx,type_d="vertical",sheet=sheet)
+    dgrid(name_text_gird_col="D",dist_gird_col="E",length_gird_col="F",coord_start=ocy,type_d="horizontal",sheet=sheet)
+run()
